360cb4fe633bb23ee6e20ef423e7c60ea43db3f7
LucasMonnanni <lmonnanni@fi.uba.ar>
2026-06-11 17:46:18 -0300

    render.com deployment

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE:-tickets.settings.demo}"
+
+python -m pip install -r requirements/demo.txt
+
+python manage.py migrate --settings="$DJANGO_SETTINGS_MODULE" --noinput
+python manage.py collectstatic --settings="$DJANGO_SETTINGS_MODULE" --noinput
